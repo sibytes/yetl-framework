de415997e8583768afead2d0ade35fb1995ce893
Shaun Ryan <Shaun_Chiburi@hotmail.com>
2021-11-23 20:21:03 +0000

    feature: prototyping decorators

--- main.py
+++ main.py
@@ -1,4 +1,52 @@
+from pyspark.sql import Row, DataFrame, SparkSession
+from pyspark.sql.functions import *
+from pyspark.sql.types import StructField, StructType, StringType, LongType
+from yetl.yetl import Yetl
 
 
+# # @yetl.test_dataset(dataset="customers")
+def get_test_customer_df(spark:SparkSession):
+    # create test dataset
+
+    test_schema = StructType([
+        StructField("id", StringType(), True),
+        StructField("firstname", StringType(), True),
+        StructField("lastname", StringType(), True)
+    ])
+
+    test_rows = [Row(1, "Terry", "Merry"), 
+            Row(2, "Berry", "Gederry"), 
+            Row(3, "Larry", "Tarry")]
+
+    test_df = spark.createDataFrame(test_rows, test_schema)
+    return test_df
 
 
+@Yetl.transform
+def transform_customer(df:DataFrame=None):
+
+    # do stranformations
+    transformed_df = (df.withColumn("Full Name", 
+        concat_ws(" ", col("firstname"), col("lastname") ))
+    )
+    return transformed_df
+
+
+# # @yetl.test_assertion(dataset="customers")
+# def yetl_test_asserts(df:DataFrame):
+#     # do assertions
+#     pass
+
+
+if __name__ == "__main__":
+
+    # spark = (SparkSession
+    #         .builder
+    #         .master("local[2]")
+    #         .appName("Spark SQL basic example")
+    #         .getOrCreate())
+
+    # df = get_test_customer_df(spark)
+
+    df = transform_customer(None)
+    df.show()
--- yetl/yetl.py
+++ yetl/yetl.py
@@ -1,3 +1,55 @@
+import functools
+from pyspark.sql import Row, DataFrame, SparkSession
+from pyspark.sql.functions import *
+from pyspark.sql.types import StructField, StructType, StringType, LongType
 
-def yetl():
-    pass
+class Yetl():
+
+    def repeat(num_times):
+        def decorator_repeat(func):
+            @functools.wraps(func)
+            def wrapper_repeat(*args, **kwargs):
+                for _ in range(num_times):
+                    value = func(*args, **kwargs)
+                return value
+            return wrapper_repeat
+        return decorator_repeat
+
+
+    def transform(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+
+            if (
+                    not (
+                        args
+                        or "df" in kwargs 
+                        or kwargs.get("df", None)
+                    )
+                    or (args and not args[0])
+                ):
+
+                # need to pass this from a test decorator
+                spark = (SparkSession
+                        .builder
+                        .master("local")
+                        .appName("Spark SQL basic example")
+                        .getOrCreate())
+
+                schema = StructType([
+                    StructField("id", StringType(), True),
+                    StructField("firstname", StringType(), True),
+                    StructField("lastname", StringType(), True)
+                ])
+
+                rows = [Row(1, "Terry", "Merry"), 
+                        Row(2, "Berry", "Gederry"), 
+                        Row(3, "Larry", "Tarry")]
+
+                df = spark.createDataFrame(rows, schema)
+            
+                return func(df)
+            else:
+                return func(*args, **kwargs)
+
+        return wrapper
