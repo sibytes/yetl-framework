74bba02d0406127c1231d4aadc00104c2347b2e6
Shaun Ryan <Shaun_Chiburi@hotmail.com>
2021-12-05 09:57:50 +0000

    feature: prototyping template loader

--- main.py
+++ main.py
@@ -14,11 +14,11 @@ from yetl.metasource.meta_source_file import MetasourceFile
 
 import jinja2
 
-templateLoader = MetasourceFile(path="./project")
+templateLoader = MetasourceFile(searchpath="./project")
 templateEnv = jinja2.Environment(loader=templateLoader)
-TEMPLATE_FILE = "project.yml"
+TEMPLATE_FILE = "datastore/test.yml"
 template = templateEnv.get_template(TEMPLATE_FILE)
-print(templateEnv.list_templates())
-# outputText = template.render()  # this is where to put args to the template renderer
+# print(templateEnv.list_templates())
+outputText = template.render()  # this is where to put args to the template renderer
 
-# print(outputText)
+print(outputText)
